fb38fd38c3713516a4b2de3fa834c160cedf269f
liuchuanqew <liuchuan.qew@163.com>
2018-05-06 20:06:29 +0800

    initial commit

---
+++ headlines.py
@@ -0,0 +1,57 @@
+import feedparser
+from flask import Flask, render_template, request
+import json
+import urllib3
+import urllib
+
+app = Flask(__name__)
+
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'sina': 'http://rss.sina.com.cn/news/society/focus15.xml',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
+DEFAULTS = {
+    'publication': 'bbc', 'city': 'London,UK'
+}
+
+WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=cb932829eacb6a0e9ee4f38bfbf112ed'
+
+@app.route("/")
+def home():
+    # get customized headlines, based on user input or default
+    publication = request.args.get("publication")
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    # get customized weather based on user input or default
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    return render_template("home.html", articles=articles, weather=weather)
+
+def get_news(query):
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = DEFAULTS["publication"]
+    else:
+        publication = query.lower()
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    return feed['entries']
+
+
+def get_weather(query):
+    query = urllib.parse.quote(query)
+    url = WEATHER_URL.format(query)
+    data = urllib.request.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description": parsed["weather"][0]["description"], "temperature": parsed["main"]["temp"],
+                   "city": parsed["name"], 'country': parsed['sys']['country']
+                   }
+    return weather
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
